e42b19ef6450610e7d1d03906ce4568385fba6ea
husanbek123 <husanbek555@gmail.com>
2023-07-20 21:52:08 +0500

    nimadir

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>2</h1>
+        <h1>12122222222</h1>
       </main>
     </>
   );
---
+++ pages/test/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+type Props = {};
+
+export default function Test({}: Props) {
+  return <div>Test a][ska[pjs[pj;aj paius[obb</div>;
+}
